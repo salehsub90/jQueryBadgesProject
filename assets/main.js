4603c25c66f032d336953bc38a01c1180d36a8e4
Saleh Suboh <salehsuboh@gmail.com>
2017-06-10 10:01:57 -0400

    Added new elements for badges

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,18 @@ $(function() {
     url: 'https://www.codeschool.com/users/salehsuboh.json',
     dataType: 'jsonp',
     success: function(response) {
-      
+      addCourses(response.courses.completed);
     }
   });
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+      $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+    });
+  }
 });
