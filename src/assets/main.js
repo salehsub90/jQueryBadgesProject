b22633208e18d3cb03e311987e3fe263d8fae2e5
Saleh Suboh <salehsuboh@gmail.com>
2017-09-03 01:49:23 +0300

    fix another bug in badges var

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,14 +9,14 @@ $(function() {
 
   function addCourses(courses) {
 
-    var badges = $('#badges');
+    var thisBadge = $('#badges');
 
     courses.forEach(function(course) {
       //console.log(course.title);
 
       var Onecourse = $('<div />', {
         'class': 'course'
-      }).appendTo(badges);
+      }).appendTo(thisBadge);
 
       $('<h3 />', {
         text: course.title
